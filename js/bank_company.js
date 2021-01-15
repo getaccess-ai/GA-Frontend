69313fd6548f7b0353a8a8e1178fe04dd74230c7
Shreyas Gupta <technology.shreyas@gmail.com>
2021-01-15 21:55:55 +0530

    bank - add company details and connection details

--- js/bank_company.js
+++ js/bank_company.js
@@ -1,7 +1,36 @@
 const baseURL = 'https://z2o.herokuapp.com';
 
-const companyHTML = (company) => {
+const detailsHTML = (company) => {
     const date = new Date(company.createdAt);
+    return `
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>ID:</b> ${company.companyId}</li>
+        <li class="list-group-item"><b>Email:</b> ${company.email}</li>
+        <li class="list-group-item"><b>Type:</b> ${company.companyType}</li>
+        <li class="list-group-item"><b>Region:</b> ${company.region}</li>
+        <li class="list-group-item"><b>SIF:</b> ${company.SIF}</li>
+        <li class="list-group-item"><b>Business Line:</b> ${company.businessLine}</li>
+        <li class="list-group-item"><b>Created:</b> ${date.toDateString()}</li>
+    </ul>
+    `
+}
+
+const connectionHTML = (company) => {
+    if(!company.dataConnection) {
+        return `
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b>This company hasn't connected an accounting software yet.</b></li>
+        </ul>
+        `;
+    }
+    return `
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>Platform:</b> ${company.dataConnection.name.charAt(0).toUpperCase() + company.dataConnection.name.slice(1)}</li>
+    </ul>
+    `
+}
+
+const companyHTML = (company) => {
     return `
             <div class="d-flex w-100 justify-content-between">
             <h1>${company.name} &nbsp; <span class="badge bg-primary rounded-pill">${company.status.charAt(0).toUpperCase() + company.status.slice(1)}</span></h1>
@@ -17,15 +46,24 @@ const companyHTML = (company) => {
             <div class="col-sm">
                 <h2>Details</h2>
                 <div class="w-100 separator mt-2 shadow-sm"></div>
+                ${detailsHTML(company)}
             </div>
             <div class="col-sm">
-                <h2>Connection</h2>
-                <div class="w-100 separator mt-2 shadow-sm"></div>
+                <div class="row">
+                    <div class="col-sm">
+                        <h2>Connection</h2>
+                        <div class="w-100 separator mt-2 shadow-sm"></div>
+                        ${connectionHTML(company)}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm">
+                        <h2>Settings</h2>
+                        <div class="w-100 separator mt-2 shadow-sm"></div>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm">
-                <h2>Settings</h2>
-                <div class="w-100 separator mt-2 shadow-sm"></div>
-            </div>
+            
             </div>
         </div>
     `;
