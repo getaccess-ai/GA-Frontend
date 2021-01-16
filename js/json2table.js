644cda9824795bae627460591feacd7380295f8a
abhiandthetruth <abhiandthetruth@gmail.com>
2021-01-16 11:31:04 +0530

    Fixed nested tables

--- js/json2table.js
+++ js/json2table.js
@@ -7,14 +7,14 @@ function buildTable(a) {
         "object" != typeof a[c] || isArray(a[c]) ? "object" == typeof a[c] && isArray(a[c]) ? (d = e.insertRow(-1),
         b = d.insertCell(-1),
         b.colSpan = 2,
-        b.innerHTML = '<div class="td_head">' + encodeText(c) + '</div><table style="width:100%">' + $(buildArray(a[c]), !1).html() + "</table>") : (d = e.insertRow(-1),
+        b.innerHTML = '<div class="td_head">' + encodeText(c) + '</div><table class="table table-bordered table-striped" style="width:100%">' + $(buildArray(a[c]), !1).html() + "</table>") : (d = e.insertRow(-1),
         b = d.insertCell(-1),
         b.innerHTML = "<div class='td_head'>" + encodeText(c) + "</div>",
         d = d.insertCell(-1),
         d.innerHTML = "<div class='td_row_even'>" + encodeText(a[c]) + "</div>") : (d = e.insertRow(-1),
         b = d.insertCell(-1),
         b.colSpan = 2,
-        b.innerHTML = '<div class="td_head">' + encodeText(c) + '</div><table style="width:100%">' + $(buildTable(a[c]), !1).html() + "</table>");
+        b.innerHTML = '<div class="td_head">' + encodeText(c) + '</div><table class="table table-bordered table-striped" style="width:100%">' + $(buildTable(a[c]), !1).html() + "</table>");
     return e
 }
 function buildArray(a) {
@@ -28,7 +28,7 @@ function buildArray(a) {
         if ("object" != typeof a[f] || isArray(a[f]))
             "object" == typeof a[f] && isArray(a[f]) ? (b = d.insertCell(h),
             b.colSpan = 2,
-            b.innerHTML = '<div class="td_head"></div><table style="width:100%">' + $(buildArray(a[f]), !1).html() + "</table>",
+            b.innerHTML = '<div class="td_head"></div><table class="table table-bordered table-striped" style="width:100%">' + $(buildArray(a[f]), !1).html() + "</table>",
             c = !0) : p || (h += 1,
             p = !0,
             b = d.insertCell(h),
@@ -53,7 +53,7 @@ function buildArray(a) {
                 c = 0; c < n; c++)
                     b = d.insertCell(c),
                     b.className = td_class,
-                    l = c == h ? '<table style="width:100%">' + $(buildArray(a[f]), !1).html() + "</table>" : " ",
+                    l = c == h ? '<table class="table table-bordered table-striped" style="width:100%">' + $(buildArray(a[f]), !1).html() + "</table>" : " ",
                     b.innerHTML = "<div class='" + td_class + "'>" + encodeText(l) + "</div>";
             else
                 for (h = m.empty,
@@ -73,7 +73,7 @@ function buildArray(a) {
                 h = m[l],
                 b = d.cells[h],
                 b.className = td_class,
-                "object" != typeof c[k] || isArray(c[k]) ? "object" == typeof c[k] && isArray(c[k]) ? b.innerHTML = '<table style="width:100%">' + $(buildArray(c[k]), !1).html() + "</table>" : b.innerHTML = "<div class='" + td_class + "'>" + encodeText(c[k]) + "</div>" : b.innerHTML = '<table style="width:100%">' + $(buildTable(c[k]), !1).html() + "</table>"
+                "object" != typeof c[k] || isArray(c[k]) ? "object" == typeof c[k] && isArray(c[k]) ? b.innerHTML = '<table class="table table-bordered table-striped" style="width:100%">' + $(buildArray(c[k]), !1).html() + "</table>" : b.innerHTML = "<div class='" + td_class + "'>" + encodeText(c[k]) + "</div>" : b.innerHTML = '<table  class="table table-bordered table-striped" style="width:100%">' + $(buildTable(c[k]), !1).html() + "</table>"
         }
     return e
 }
