bb2122065af52041f97e9067d1724b8c5df265e6
Shreyas Gupta <technology.shreyas@gmail.com>
2021-02-03 12:13:34 +0530

    admin login and bank onboard

---
+++ js/admin_login.js
@@ -0,0 +1,32 @@
+const baseURL = 'https://z2o.herokuapp.com';
+const loginForm = document.getElementById('login-form');
+
+const adminKey = Cookies.get('adminKey');
+
+if (typeof adminKey !== 'undefined') {
+    document.location.href = 'admin_dashboard.html';
+}
+
+$(".page-loader").fadeOut('fast');
+
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    $(".page-loader").fadeIn('fast');
+    const data = new FormData(loginForm);
+    const loginBody = {
+        adminId: data.get('adminId'),
+        password: data.get('password')
+    };
+    axios.post(baseURL + '/admin/auth', loginBody)
+        .then(response => {
+            console.log(response.data);
+            Cookies.set('adminKey', response.data.authKey);
+            Cookies.set('adminId', response.data.data.adminId);
+            document.location.href = 'admin_dashboard.html';
+        })
+        .catch(error => {
+            console.log(error);
+            $(".page-loader").fadeOut('fast');
+            alert(error.response.data.error || "AdminID or Password didn't validate");
+        })
+})
