95c2ab197d468b0edfff56ef4a5188286ad39bf7
abhiandthetruth <abhiandthetruth@gmail.com>
2021-03-14 19:07:41 +0530

    Fixed ordering

--- js/bank_dashboard.js
+++ js/bank_dashboard.js
@@ -6,8 +6,8 @@ let mp = {
   0: "Total",
   1: "NotConnected",
   2: "Connected",
-  3: "Active",
-  4: "NotActive",
+  3: "NotActive",
+  4: "Active",
 };
 const handleclick = (evt) => {
   let activeElement = chart.getElementAtEvent(evt);
@@ -16,7 +16,7 @@ const handleclick = (evt) => {
 };
 
 const getAction = (ind) => {
-  const vbtn = `<a href="#">View Reports</a>`;
+  const vbtn = `<a href="bank_company.html?companyId=${custList.mapping[ind]}">View Reports</a>`;
   const rbtn = `<a href="#">Send Reminder</a>`;
   if (curIdx == 4 || custList["Active"].data.includes(ind)) return vbtn;
   else if (
@@ -51,152 +51,102 @@ const renderTable = (obj) => {
   }
 };
 
-const loadCompanies = () => {
+const loadCompanies = async () => {
   axios.defaults.headers.common["Authorization"] = Cookies.get("authKey");
-  axios
-    .get(baseURL + "/bank/companies")
-    .then((response) => {
-      let ctx = document.getElementById("waterfallChart").getContext("2d");
-      document
-        .getElementById("waterfallChart")
-        .addEventListener("click", handleclick);
+  let ctx = document.getElementById("waterfallChart").getContext("2d");
+  document
+    .getElementById("waterfallChart")
+    .addEventListener("click", handleclick);
 
-      custList = {
-        Total: {
-          count: 10,
+  const resp = await axios.get(
+    "https://api-getaccess.herokuapp.com/bank/aggregations/companyBreakup"
+  );
+  console.log(resp);
+  custList = resp.data;
+  renderTable(custList.Total.data);
+  const total = custList["Total"].count,
+    notConnected = custList["NotConnected"].count,
+    connected = custList["Connected"].count,
+    notActive = custList["NotActive"].count,
+    active = custList["Active"].count;
+  chart = new Chart(ctx, {
+    responsive: true,
+    maintainAspectRatio: false,
+    type: "bar",
+    data: {
+      labels: [
+        "Total Added",
+        "Not Connected",
+        "Connected",
+        "Not Active",
+        "Active",
+      ],
+      datasets: [
+        {
           data: [
-            "ACME Industries",
-            "Z2O",
-            "Archana Ind",
-            "Triple A",
-            "Croatia Biz",
-            "Precious Stones",
-            "Monsters INC",
-            "Creative PVT Limited",
-            "SMPT Ind",
-            "Vertigo Pvt.",
+            total,
+            [connected, total],
+            [0, connected],
+            [active, connected],
+            [0, active],
           ],
-        },
-        Connected: {
-          count: 7,
-          data: [
-            "ACME Industries",
-            "Z2O",
-            "Triple A",
-            "Croatia Biz",
-            "Monsters INC",
-            "SMPT Ind",
-            "Vertigo Pvt.",
+          backgroundColor: [
+            "#b03060",
+            "#b7446f",
+            "#bf597f",
+            "#c76e8f",
+            "#cf829f",
           ],
+          barPercentage: 1,
         },
-        NotConnected: {
-          count: 3,
-          data: ["Archana Ind", "Precious Stones", "Creative PVT Limited"],
-        },
-        Active: {
-          count: 6,
-          data: [
-            "ACME Industries",
-            "Z2O",
-            "Triple A",
-            "Croatia Biz",
-            "Monsters INC",
-            "SMPT Ind",
-          ],
-        },
-        NotActive: {
-          count: 1,
-          data: ["Vertigo Pvt."],
-        },
-      };
-      renderTable(custList.Total.data);
-      const total = custList["Total"].count,
-        notConnected = custList["NotConnected"].count,
-        connected = custList["Connected"].count,
-        notActive = custList["NotActive"].count,
-        active = custList["Active"].count;
-      chart = new Chart(ctx, {
-        responsive: true,
-        maintainAspectRatio: false,
-        type: "bar",
-        data: {
-          labels: [
-            "Total Added",
-            "Not Connected",
-            "Connected",
-            "Not Active",
-            "Active",
-          ],
-          datasets: [
-            {
-              data: [
-                total,
-                [connected, total],
-                [0, connected],
-                [active, connected],
-                [0, active],
-              ],
-              backgroundColor: [
-                "#b03060",
-                "#b7446f",
-                "#bf597f",
-                "#c76e8f",
-                "#cf829f",
-              ],
-              barPercentage: 1,
+      ],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
             },
-          ],
-        },
-        options: {
-          legend: {
-            display: false,
           },
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        },
-      });
+        ],
+      },
+    },
+  });
 
-      let ctx1 = document.getElementById("periodChart").getContext("2d");
-      let chart1 = new Chart(ctx1, {
-        responsive: true,
-        maintainAspectRatio: false,
-        type: "bar",
-        data: {
-          labels: ["Sep-20", "Oct-20", "Nov-20", "Dec-20", "Jan-21", "Feb-21"],
-          datasets: [
-            {
-              data: [10, 9, 15, 8, 8, 10],
-              barPercentage: 1,
+  let ctx1 = document.getElementById("periodChart").getContext("2d");
+  let chart1 = new Chart(ctx1, {
+    responsive: true,
+    maintainAspectRatio: false,
+    type: "bar",
+    data: {
+      labels: ["Sep-20", "Oct-20", "Nov-20", "Dec-20", "Jan-21", "Feb-21"],
+      datasets: [
+        {
+          data: [10, 9, 15, 8, 8, 10],
+          barPercentage: 1,
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
             },
-          ],
-        },
-        options: {
-          legend: {
-            display: false,
           },
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        },
-      });
-      $(".content-loader").fadeOut("fast");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+        ],
+      },
+    },
+  });
+  $(".content-loader").fadeOut("fast");
 };
 
 loadCompanies();
